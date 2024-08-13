1eb6a950147f8fb08eb0cf691e996a0879538e66
SoCLoN666 <kusminoleh96@gmail.com>
2024-08-13 22:24:57 +0300

    clean up

--- e2e/keywords/navigation/toggleSidebar.ts
+++ e2e/keywords/navigation/toggleSidebar.ts
@@ -1,12 +1,13 @@
 import { Page } from "@playwright/test";
+import { SidebarIds } from "e2e/selectors/sidebar";
 
 export async function toggleSidebar(page: Page, state: "open" | "closed"): Promise<void> {
   const toggleState = await getToggleState(page);
 
   if (state === "open" && toggleState === "hidden") {
-    await page.locator('[id="sidebar-toggle-copy"]').click();
+    await page.locator(SidebarIds.CollapsedSidebarToggle).click();
   } else if (state === "closed" && toggleState === "visible") {
-    await page.locator('[id="sidebar"]').click();
+    await page.locator(SidebarIds.ExpandedSidebarToggle).click();
   } else {
     return;
   }
